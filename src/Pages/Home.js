b0abfbab4e40db000c028704779791b5ed424708
AyoubTahar <tahar.ayoub@gmail.com>
2023-02-02 15:43:57 +0100

    ajout loader

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Loader from '../components/Loader';
 import ThumbnailHouse from '../components/ThumbnailHouse';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
     return (
             <div>
             <Header/>
+            <Loader/>
             <Banner />
             <ThumbnailHouse/>
             <Footer/>
