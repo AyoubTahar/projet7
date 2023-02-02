833aaedc43205a27d6d28f3dce42538b0398ae0a
AyoubTahar <tahar.ayoub@gmail.com>
2023-02-02 13:27:36 +0100

    changement format image bannière

--- src/Pages/FicheLogement.js
+++ src/Pages/FicheLogement.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useLocation, Navigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import ImageSlider from '../components/ImageSlider';
